3890d8bb9e711719a6f1535c15b5392439973e82
hughlilly <hughlilly@users.noreply.github.com>
2021-08-18 17:46:59 +1200

    First version of downloader

---
+++ flickrDownloader.py
@@ -0,0 +1,97 @@
+
+import csv
+import logging
+import xml.etree.ElementTree as ET
+
+import flickrapi
+from decouple import config
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+logfile_format = logging.Formatter(
+    '%(asctime)s: %(name)s (%(funcName)s): %(message)s')
+logfile = logging.FileHandler(filename='flickrDownload.log', encoding='utf8')
+logfile.setFormatter(logfile_format)
+logfile.setLevel(logging.DEBUG)
+
+log.addHandler(stream_handler)
+log.addHandler(logfile)
+
+flickrKey = config('FLICKR_KEY')
+flickrSecret = config('FLICKR_SECRET')
+
+# Create flickr object from the FlickrAPI constructor. Get parsed JSON,
+# and save the token in a folder relative to this script. Use caching.
+# On Windows, set the location to ".\\.flickr"
+flickr = flickrapi.FlickrAPI(
+    api_key=flickrKey, secret=flickrSecret,
+    cache=True, token_cache_location="./.flickr")
+
+
+def login():
+    """Login to Flickr and report some basic user info."""
+    current_user = flickr.test.login()
+    username = current_user.find('user')[0].text
+    id = current_user.find('user').attrib['id']
+    log.info("Logged in as {0} ({1})".format(username, id))
+
+    user_info = flickr.people.getInfo(user_id=id)
+    upload_count = user_info.find('.//count').text
+    views = user_info.find('.//views').text
+    log.info("{0} photos; {1} views".format(upload_count, views))
+
+    get_data(user_identifier=id)
+
+def auth_check():
+    """Check if a token exists (in .flickr), otherwise obtain one."""
+    permissions = 'write'
+    if not flickr.token_valid(perms=permissions):
+        log.info("No valid token stored. Getting a new one.")
+        flickr.get_request_token(oauth_callback='oob')
+
+        # Print the Authorisation URL, and ask the user to open it.
+        authorisation_url = flickr.auth_url(perms=permissions)
+        log.info("Authorisation URL:\n{0}\n".format(authorisation_url))
+        print("Open this URL in a web browser, authorise this app, and you should see a verifier code in the format nnn-nnn-nnn.")
+
+        # Get the verifier code from the user.
+        verification_code = str(input(
+            "Paste the code here: "))
+
+        # Trade the request token for an access token
+        flickr.get_access_token(verification_code)
+    login()
+
+def get_data(user_identifier: str):
+    print("Get data starts here.")
+    walker = flickr.walk_user(user_id=user_identifier, extras="description, tags, machine_tags, path_alias, url_o")
+    with open("flickrResults.csv", mode='a', newline='', encoding='utf-8') as output:
+        write = csv.writer(output)
+        for photo in walker:
+            attr = photo.attrib
+            id = attr['id']
+            title = attr['title']
+            tags = attr['tags']
+            mach_tags = attr['machine_tags']
+            path = attr['path_alias']
+            url_o = attr['url_o']
+            # Use repr on description bc it contains newlines that would break the csv
+            description = repr(photo.find('description').text)
+            write.writerow([id, title, description, tags, mach_tags, path, url_o])
+
+if __name__ == "__main__":
+    print("\nFlickr downloader.")
+
+    with open("flickrResults.csv", mode='w', newline='', encoding='utf-8') as output:
+        # Write headers to CSV file
+        headers = ["flickr_id", "title", "desc", "tags", "machine_tags", "path", "flickr_url"]
+        write = csv.writer(output)
+        write.writerow(headers)
+        output.close()
+
+    print(80 * "=")
+    auth_check()
